605f7e70cb071487de1d77190e18294c73ccf783
Shaun Savage <savages@savages.com>
2024-12-07 21:22:58 +0000

    Changed links bindgen is moreimportant than typescript

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
-import wasmpackLogo from '/rustwasm.png'
+import wasmpackLogo from '/wasm-ferris.png' 
+import bindgen from '/rustwasm.png'
 import rustferris from '/rustacean-flat-happy.svg'
 import { setupCounter } from './counter.ts'
 import * as wasm from '../pkg'
@@ -10,19 +10,19 @@ import * as wasm from '../pkg'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-   <a href="https://rustwasm.github.io/wasm-pack/installer/" target="_blank">
+   <a href="https://rustwasm.github.io//" target="_blank">
       <img src="${wasmpackLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
     <a href="https://www.rust-lang.org/" target="_blank">
       <img src="${rustferris}" class="logo vanilla" alt="TypeScript logo" />
     </a>
-    <a href="https://vite.dev" target="_blank">
+    <a href="https://github.com/rustwasm/wasm-bindgen" target="_blank">
+      <img src="${bindgen}" class="logo vanilla" alt="TypeScript logo" />
+    </a>
+   <a href="https://vite.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-     <h1>WASM + Rust + Vite + TypeScript</h1>
+     <h1>WASM + Rust + bindgen + Vite</h1>
        <p class="read-the-docs">
       Click on any logo to learn more
     </p>
